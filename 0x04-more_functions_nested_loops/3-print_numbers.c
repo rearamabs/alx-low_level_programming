828fd16121ad99be3ee1890f167dffd90019658e
rearamabs <tumeloramaboea454@gmail.com>
2023-09-14 08:22:28 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print numbers from 0 to 9
- * Return: Always 0
+ * Return:Always 0
  */
 
 void print_numbers(void)
--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -10,4 +10,3 @@ int main(void)
     print_most_numbers();
     return (0);
 }
-
